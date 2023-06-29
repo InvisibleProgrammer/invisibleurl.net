7695104d38691eccc4c7fbee8b18df1521437fde
Zsolt Miskolczi <zsolt.miskolczi@gmail.com>
2023-06-29 21:42:07 +0200

    Test out redirect functionality

--- main.go
+++ main.go
@@ -7,8 +7,21 @@ import (
 func main() {
 	app := fiber.New()
 
+	shortUrl := "blog"
+	longUrl := "https://invisibleprogrammer.com"
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString(("The best URL Shortener ever"))
+		return c.SendString("The best URL Shortener ever")
+	})
+
+	app.Get("/:shortUrl", func(c *fiber.Ctx) error {
+		short := c.Params("shortUrl")
+
+		if short == shortUrl {
+			return c.Redirect(longUrl, fiber.StatusFound)
+		}
+
+		return c.SendString("waaat")
 	})
 
 	app.Listen(":3000")
